68423b161f87943b57634a1a0cbb72474ed9c9fb
aslambaba <42738148+aslambaba@users.noreply.github.com>
2020-08-26 06:46:21 -0700

    Initial commit

---
+++ server_site/index.js
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const Customers = [
+    {
+        "Name": 'Ted Ford',
+        "Product": 'IPhone X',
+        "Country": 'USA',
+    },
+    {
+        "Name": 'Aslam',
+        "Product": 'IPhone 5',
+        "Country": 'Pakistan',
+    },
+    {
+        "Name": 'Rimi',
+        "Product": 'IPhone 8',
+        "Country": 'UK',
+    },
+];
+const resolvers = {
+    Query: {
+        Customers: () => Customers,
+    },
+};
+
+const typeDefs = gql`
+  
+  type Customer {
+    Name: String
+    Product: String
+    Country: String
+  }
+  
+  type Query {
+    Customers: [Customer]
+  }
+`;
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
