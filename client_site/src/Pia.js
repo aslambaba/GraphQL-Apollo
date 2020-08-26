d171b322180d7d2210d6f7c8d72bc4d3c34ccaf0
aslambaba <42738148+aslambaba@users.noreply.github.com>
2020-08-26 10:14:24 -0700

    Client-site Setup
    client site setup n  graphql with appollo server

---
+++ client_site/src/Pia.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Pia() {
+
+    return (
+        <div>
+            <h3>Aslam Baba </h3>
+        </div>
+    )
+}
+
+export default Pia;
---
+++ client_site/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pia from './Pia.js';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Pia />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
